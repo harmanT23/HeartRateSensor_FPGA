4203522408191e867278db9692be6ca9230160cd
harmanT23 <harmant@live.com>
2017-04-08 21:42:49 -0400

    Update heartSensorMain.s

--- heartSensorMain.s
+++ heartSensorMain.s
@@ -11,21 +11,23 @@ _start:
 	stwio r2, 0(r3) #Set ADC to auto update
 
 	Main_Loop:
-		call Poll_ADC_CH0 #Get the current ADC value
+		call Get_ADC_CH0 #Get the current ADC value
 		mov r4, r2
 		call Print_Raw_Value
+		br Main_Loop
 
 
 
-
-Poll_ADC_CH0:
+Get_ADC_CH0:
 	movia r7, ADC_CH0
 	ldw r2, 0(r7) #Read the value from ADC channel 0 
 	ret #return 
 
 Print_Raw_Value:
 	movia r7, 0xFF201000 #The base Address for the JTAG UART
-	stwio r4, 0(r7) #send 1 bit of information to JTAG UART
+	ldb r6, 0(r4) #load a byte from r4 into r6
+	addi r6, r6, 0x30  #take the number and add the character '0' to it
+	stwio r6, 0(r7) #send 1 bit of information to JTAG UART
 	srli r4, r4, 1 #Shift r4 by 1 bit to the right until it is all 0's
 	bne r4, r0, Print_Raw_Value
 	ret
