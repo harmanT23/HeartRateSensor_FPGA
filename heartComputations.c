0ed4f49f49eedc7b01c9dec27a0cc568c1fc5fdd
harmanT23 <harmant@live.com>
2017-04-11 00:48:10 -0400

    Delete heartComputations.c

--- heartComputations.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int Heart_Rate(int timedCycles, int totalTimeCycles, int BoardCycles, int minPerSec) {
-	int numCycles = totalTimeCycles - timedCycles;
-	int period = numCycles/BoardCycles;
-	int BPM = minPerSec/period;
-	return BPM;
-}
-
