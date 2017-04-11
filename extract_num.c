abb60ddc361e67a2368185635954e5640cb055e4
kamathmanu <manuriddle@gmail.com>
2017-04-11 04:50:13 -0400

    Update extract_num.c

--- extract_num.c
+++ extract_num.c
@@ -5,14 +5,23 @@
 //  Created by KAMATH Manukiran on 11/04/2017.
 //  Copyright © 2017 KAMATH Manukiran. All rights reserved.
 //
-
 #include <stdio.h>
 #include <math.h>
 
-int extract_num(unsigned int num_to_be_extracted);
-
-int extract_num (unsigned int num_to_be_extracted){
+int extract_digit0 (unsigned int num_to_be_extracted){
     
     return (num_to_be_extracted % 10);
+    
+}
+
+int extract_digit1 (unsigned int num_to_be_extracted){
+    
+    return (num_to_be_extracted % 100);
+    
+}
+
+int extract_digit2 (unsigned int num_to_be_extracted){
+    
+    return (num_to_be_extracted % 100);
     
 }
